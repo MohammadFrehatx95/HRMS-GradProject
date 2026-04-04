342f373630bfa93d6d3f40893669695f0d45d40c
Abood <abedelqaberalfaqeeh@gmail.com>
2026-04-04 22:39:53 +0300

    FirstComm

---
+++ HRMS-GradProject/Entity/Department.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRMS_GradProject.Entity
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public string location { get; set; }
+
+        // Relationships
+        public ICollection<Employee> Employees { get; set; } 
+
+
+
+
+
+    }
+}
