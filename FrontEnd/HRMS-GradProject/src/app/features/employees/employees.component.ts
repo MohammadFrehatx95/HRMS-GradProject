f414f5717299f0f2f157c74ce133a570db304ecd
Mohammad Alghazo <mohammadalghazo2004m@gmail.com>
2026-05-02 19:25:43 +0300

    Init Employees Table

--- FrontEnd/HRMS-GradProject/src/app/features/employees/employees.component.ts
+++ FrontEnd/HRMS-GradProject/src/app/features/employees/employees.component.ts
@@ -4,8 +4,30 @@ import { Component } from '@angular/core';
   selector: 'app-employees',
   imports: [],
   templateUrl: './employees.component.html',
-  styleUrl: './employees.component.css'
+  styleUrl: './employees.component.css',
 })
 export class EmployeesComponent {
-
+  employeesList = [
+    {
+      id: 1,
+      name: 'Ahmad Salem',
+      position: 'Frontend Developer',
+      department: 'IT',
+      status: 'Active',
+    },
+    {
+      id: 2,
+      name: 'Sara Ali',
+      position: 'HR Manager',
+      department: 'HR',
+      status: 'On Leave',
+    },
+    {
+      id: 3,
+      name: 'Omar Zaid',
+      position: 'Backend Developer',
+      department: 'IT',
+      status: 'Active',
+    },
+  ];
 }
