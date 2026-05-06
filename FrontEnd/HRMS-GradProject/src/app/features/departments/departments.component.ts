548229bb501932aed6e27aa673a7e1c31805a0f6
Mohammad Alghazo <mohammadalghazo2004m@gmail.com>
2026-05-07 01:09:05 +0300

    Update Departments

--- FrontEnd/HRMS-GradProject/src/app/features/departments/departments.component.ts
+++ FrontEnd/HRMS-GradProject/src/app/features/departments/departments.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DepartmentService } from '../../core/services/department.service';
+import Swal from 'sweetalert2';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-departments',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './departments.component.html',
 })
 export class DepartmentsComponent implements OnInit {
+  private departmentService = inject(DepartmentService);
+
   departmentsList: any[] = [];
   isLoading: boolean = true;
-  private departmentService = inject(DepartmentService);
+
+  selectedDepartment: any = null;
+  private detailsModal: any;
 
   ngOnInit() {
     this.loadDepartments();
   }
 
   loadDepartments() {
+    this.isLoading = true;
     this.departmentService.getDepartments().subscribe({
       next: (data) => {
         this.departmentsList = data;
@@ -26,5 +36,17 @@ export class DepartmentsComponent implements OnInit {
         this.isLoading = false;
       },
     });
+  }
+
+  viewDetails(dept: any) {
+    this.selectedDepartment = dept;
+
+    setTimeout(() => {
+      const modalElement = document.getElementById('deptDetailsModal');
+      if (modalElement) {
+        this.detailsModal = new bootstrap.Modal(modalElement);
+        this.detailsModal.show();
+      }
+    }, 0);
   }
 }
