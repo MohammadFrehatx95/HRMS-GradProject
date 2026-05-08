9838c7bd626a96721986357026af61bee58774b3
Abood <abedelqaberalfaqeeh@gmail.com>
2026-05-08 22:52:58 +0300

    DB Update

---
+++ Infrastructure/Migrations/20260508192900_3FinalEdit.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class _3FinalEdit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Leaves_Employees_EmployeeId",
+                table: "Leaves");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Leaves_Employees_EmployeeId",
+                table: "Leaves",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Leaves_Employees_EmployeeId",
+                table: "Leaves");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Leaves_Employees_EmployeeId",
+                table: "Leaves",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
