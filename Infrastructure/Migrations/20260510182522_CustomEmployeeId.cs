3870a31b6a5fbe1d88f62be118a0bcd32dd633ef
Abood <abedelqaberalfaqeeh@gmail.com>
2026-05-10 22:03:47 +0300

    CustomEmployeeId

---
+++ Infrastructure/Migrations/20260510182522_CustomEmployeeId.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomEmployeeId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Employees",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "HR");
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "IT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Employees",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Human Resources");
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Information Technology");
+        }
+    }
+}
