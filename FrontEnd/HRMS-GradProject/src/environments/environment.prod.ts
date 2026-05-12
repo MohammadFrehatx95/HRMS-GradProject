cd654289137ad3358a73ee3e28ac82297f5305ee
Mohammad Alghazo <mohammadalghazo2004m@gmail.com>
2026-05-12 22:01:52 +0300

    edit domain

--- FrontEnd/HRMS-GradProject/src/environments/environment.prod.ts
+++ FrontEnd/HRMS-GradProject/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // TODO: Replace with your actual Render URL later
-  apiUrl: 'https://hrms-gradproject.onrender.com/api'
+  apiUrl: 'https://kawadir-hrms.onrender.com/api'
 };
